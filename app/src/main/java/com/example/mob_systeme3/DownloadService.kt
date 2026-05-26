c695f16e2b07d66ea4c2fe7abc15bf0837f69ed4
evycham <ygruzevich8484@gmail.com>
2026-05-27 01:54:09 +0200

    Notification

--- app/src/main/java/com/example/mob_systeme3/DownloadService.kt
+++ app/src/main/java/com/example/mob_systeme3/DownloadService.kt
@@ -1,15 +1,47 @@
 package com.example.mob_systeme3
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class DownloadService : Service() {
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+    private val CHANNEL_ID = "download_channel"
 
-        Log.d("Test", "Service test")
+    private fun createNotificationChannel(){
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            "Download Service",
+            NotificationManager.IMPORTANCE_LOW
+        )
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        createNotificationChannel()
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(android.R.drawable.stat_sys_download)
+            .setContentTitle("Download läuft")
+            .setContentText("Datei wird heruntergeladen...")
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .build()
+
+        startForeground(1, notification)
+
+
+
 
         return START_STICKY
     }
